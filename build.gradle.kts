d11e470cc252e2b1046c4db43ec2dba63e1f0bc9
OszkarSemerath <semerath@mit.bme.hu>
2025-08-08 17:48:00 +0200

    Communication records messages from ROS

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,7 @@ version = "1.0-SNAPSHOT"
 dependencies {
     implementation(refinery.generator)
     implementation("org.example:mapConverterToRefinery:1.0-SNAPSHOT")
+    //implementation("org.eclipse.jetty.websocket:jetty-websocket-jetty-client")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
