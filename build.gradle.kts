0bf7455e03eab443c3948115a2290446b03a2150
OszkarSemerath <semerath@mit.bme.hu>
2025-08-11 21:30:59 +0200

    Visualization update with objects

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(refinery.generator)
     implementation("org.example:mapConverterToRefinery:1.0-SNAPSHOT")
     //implementation("org.eclipse.jetty.websocket:jetty-websocket-jetty-client")
+    implementation("com.github.weisj:jsvg:latest.integration")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
