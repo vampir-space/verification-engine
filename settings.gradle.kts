f45be8056922933dbc73c342e2dfa595761f3559
OszkarSemerath <semerath@mit.bme.hu>
2026-02-04 16:35:55 +0100

    Small updates and fixes in ModelSeedFragment translator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "verification-engine"
 
 plugins {
-    id("tools.refinery.settings") version "0.1.6"
+    id("tools.refinery.settings") version "0.2.0"
 }
 
 includeBuild("./refinery-map/mapConvertertToRefinery/mapConverterToRefinery")
