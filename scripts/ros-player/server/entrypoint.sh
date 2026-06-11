372345a3a3c18ae101edf029a672bcfde8b6001c
Csanád Telbisz <csanadtelbisz@gmail.com>
2026-06-11 18:28:03 +0200

    keep_last 1 dds profile

--- scripts/ros-player/server/entrypoint.sh
+++ scripts/ros-player/server/entrypoint.sh
@@ -2,15 +2,16 @@
 set -e
 
 export PYTHONUNBUFFERED=1
+export FASTDDS_DEFAULT_PROFILES_FILE=/config/dds_profile.xml
 
 source /opt/ros/setup.bash
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 ROS_PID=$!
 
-python3 /opt/sync/sync_node.py &
-SYNC_PID=$!
+#python3 /opt/sync/sync_node.py &
+#SYNC_PID=$!
 
 python3 /opt/web/app.py &
 WEB_PID=$!
 
-wait $ROS_PID $SYNC_PID $WEB_PID
+wait $ROS_PID $WEB_PID #$SYNC_PID
